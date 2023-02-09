8f578c776fed16b7d511228830ce52bea74e0222
raimundolourial <raimundolourival@gmail.com>
2023-02-09 15:58:39 -0300

    Terminei as atividades de String

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+const nome = (prompt("Digite seu nome"))
+const comida1 = (prompt("Digite a primeira comida preferida"))
+const comida2 = (prompt("Digite a segunda comida preferida"))
+const comida3 = (prompt("Digite a terceira comida preferida"))
+
+console.log(`Estas são as comidas favoritas de ${nome}:
+
+-${comida1}
+
+-${comida2}
+
+-${comida3}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log('A quantidade de caracteres antes do trim é:' + minhaString.length)
+let novaString = minhaString.trim()
+console.log('A quantidade de caracteres depois do trim é:' + novaString.length)
+
+console.log(minhaString)
+
+novaString = minhaString.replace("________", "sticioso")
+
+console.log(novaString)
